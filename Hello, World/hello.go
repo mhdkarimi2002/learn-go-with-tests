abf28d885c618aa4df71c4a23d508032e31f4696
mhdkarimi2002 <mhdkarimi2002@gmail.com>
2025-08-21 16:46:56 +0330

    init

---
+++ Hello, World/hello.go
@@ -0,0 +1,28 @@
+package main
+
+const spanish = "Spanish"
+const french = "French"
+
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjur, "
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return prefix
+}
